f81d4dc3ecc79d68e6aa695a135df84be0910f2c
Elen Magalhães <fromeleen@gmail.com>
2022-04-21 20:53:25 -0300

    Isolando componentes de Input e Button

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  background: #232129;
+  color: #666360;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    flex: 1;
+
+    border: 0;
+    background: transparent;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
