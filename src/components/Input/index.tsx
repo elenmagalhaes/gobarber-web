b56b9b03462474e8a6536a2ceb4252c2db584872
Elen Magalhães <fromeleen@gmail.com>
2022-04-21 21:50:54 -0300

    Adicionando unform e melhorando usabilidade da tela de cadastro

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,12 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
@@ -8,11 +15,41 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...props }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...props} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ icon: Icon, name, ...props }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [_isFocued, _setIsFocused] = useState(false);
+  const [_isFilled, _setIsFilled] = useState(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    _setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    _setIsFocused(false);
+    _setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFilled={_isFilled} isFocused={_isFocued}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...props}
+      />
+    </Container>
+  );
+};
 
 export default Input;
